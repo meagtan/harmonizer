802d8ca510b3cd0274d0064a5c316c1c8d1ec344
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2017-01-14 06:32:32 +0300

    Fixed normalization of probabilities

--- freqs.py
+++ freqs.py
@@ -1,6 +1,6 @@
 from music21     import *
-from collections import defaultdict
 from frequtils   import *
+from collections import defaultdict
 
 class Freq:
     'Multiple dimensional frequency table.'
@@ -8,18 +8,23 @@ class Freq:
         self.table = {} # should have a None entry keeping count, and other entries frequency tables
         self.table[None] = 0 # perhaps subclass int for this
     
-    # if first element of item is True, normalizes rest over all elements
     def __getitem__(self, item): 
         try:
-            if not item:
+            if not item: # might also store 0
                 return self.table[None]
             if not isinstance(item, tuple):
                 return self.table[item].table[None]
             if item[0] is True: # normalization
-                return Prob(self.__getitem__(item[1:]), self.table[None])
+                if len(item) == 1:
+                    return 1
+                s = 0
+                for i in self.table:
+                    if i is not None:
+                        s += self.table[i][item[2:]]
+                return Prob(self.__getitem__(item[1:]), s)
             if item[0]:
-                return self.table[item[0]][item[1:]] # TODO normalize if norm and return 0 if not member
-            return self.table[None] # TODO normalize
+                return self.table[item[0]][item[1:]]
+            return self.table[None]
         except KeyError:
             return 0
     
@@ -55,7 +60,10 @@ class Env:
     def train(self, filenames):
         'Train probabilities from given iterator of filenames, for example corpus.getBachChorales().'
         for f in filenames:
-            self.process(Sample(f))
+            s = Sample(f)
+            if s not in self.samples[None]:
+                print 'Processing %s...' % f
+                self.process(s)
     
     def process(self, sample):
         'Update probabilities based on a sample sequence of chords.'
@@ -75,10 +83,14 @@ class Env:
             f, f1 = Func(curr, key), Func(prev, key)
             self.cfreq[f, sample] += 1     
             self.tfreq[f, f1, sample] += 1 
-            for voice, n in enumerate(curr.pitches):
-                n1 = prev and voice in prev.pitches and prev.pitches[voice] # what if multiple notes are played on the same chord? consider ties
-                self.nfreq[n, f, voice, sample] += 1
-                self.vfreq[n, n1, f, f1, voice, sample] += 1
+            for voice, n in enumerate(curr.pitches): # TODO sort pitches first
+                try:
+                    n1 = prev.pitches[voice] # what if multiple notes are played on the same chord? consider ties
+                    t, t1 = Tone(n, curr), Tone(n1, prev)
+                    self.nfreq[t, f, voice, sample] += 1
+                    self.vfreq[t, t1, f, f1, voice, sample] += 1
+                except:
+                    pass
             prev = curr
     
     # TODO memoize the following
@@ -89,14 +101,14 @@ class Env:
     
     def nprob(self, n, c, k, v = None):
         'nprob(n, c, k[, v]) -> Return probability of note n occurring in voice v of chord c in key k.'
-        return self.nfreq[True, Tone(n, c), Func(c, k), v] / self.cprob(c, k)
+        return self.nfreq[True, Tone(n, c), Func(c, k), v] / self.cprob(c, k) # should normalize over v if not none
     
     def tprob(self, c1, c, k, vel = None):
         'tprob(c1, c, k[, vel]) -> Return probability of chord c changing to chord c1 in key k under harmonic velocity vel.'
         if c == c1:
             return self.cprob(c, k) # perhaps dependent on vel
         f, f1 = Func(c, k), Func(c1, k)
-        return sum(self.tfreq[True, f1, f, s] / self.cfreq[True, f, s] for s in self.samples[vel])
+        return sum(self.tfreq[True, f1, f, s] / self.cfreq[True, f, s] for s in self.samples[vel]) / len(self.samples[vel])
     
     def vprob(self, n1, n, c1, c, k, v = None, vel = None):
         '''vprob(n1, n, c1, c, k[, v, vel]) -> 
@@ -105,6 +117,6 @@ class Env:
         if n == n1 and c == c1:
             return self.nprob(n, c, k, v) # perhaps dependent on vel
         f, f1 = Func(c, k), Func(c1, k)
-        t, t1 = Tone(n, f), Tone(n1, f1)
+        t, t1 = Tone(n, c), Tone(n1, c1)
         return sum(self.vfreq[True, t1, t, f1, f, v, s] * self.cfreq[True, f, s] / (self.nfreq[True, t, f, v, s] * self.tfreq[True, f1, f, s]) 
-                   for s in self.samples[vel])
+                   for s in self.samples[vel]) / len(self.samples[vel])
