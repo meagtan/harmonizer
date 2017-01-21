4a8c1adfe2dfde6304d9085e7f5e45dc1fc201ee
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2017-01-21 22:42:57 +0300

    Added accidentals to Func

--- frequtils.py
+++ frequtils.py
@@ -1,10 +1,8 @@
 from music21 import *
 from fractions import Fraction
 
-fifths = scale.intervalNetwork.BoundIntervalNetwork(['P5'] * 12)
-tones = [str(p)[:-1] for p in fifths.realizePitch(pitch.Pitch('G-0'))]
-keys = [k for maj in map(key.Key, tones) for k in [maj, key.Key(maj.getRelativeMinor().tonic, 'minor')]]
-# keys = [k for maj in map(key.Key, tones) for k in [maj, maj.relative]]
+tones = [n + a for n in 'CDEFGABcdefgab' for a in ['-', '#', '']]
+keys = map(key.Key, tones)
 chords = [chord.Chord(map(k.pitches.__getitem__, [0, 2, 4])) for k in keys]
 
 class Func(tuple):
@@ -13,8 +11,10 @@ class Func(tuple):
     '''
     def __new__(cls, chord, key):
         try:
-            return tuple.__new__(cls, ((chord.root().diatonicNoteNum - key.tonic.diatonicNoteNum) % 7 + 1,
-                                       chord.quality, key.mode))
+            num = (chord.root().diatonicNoteNum - key.tonic.diatonicNoteNum) % 7
+            acc = (chord.root().ps - key.pitches[num].ps) % 12
+            
+            return tuple.__new__(cls, (num, acc, chord.quality, key.mode))
         except AttributeError:
             return ()
     pass
@@ -39,7 +39,7 @@ class Sample:
         self.cs = corpus.parse(filename).chordify()
         ks = self.cs.getKeySignatures()[0]
         
-        self.key = key.Key(ks.getScale().tonic, ks.mode) # ks.asKey(ks.mode)
+        self.key = key.Key(ks.getScale().tonic, ks.mode)
         self.vel = None # TODO change this, perhaps use qualities other than vel, such as measure ends
     
     def chords(self):
