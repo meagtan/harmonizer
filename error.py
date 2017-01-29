2abacc7d0f942a1408ed1f1696e96e59a3e062db
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2017-01-29 08:56:11 +0300

    Added empirical error function

--- error.py
+++ error.py
@@ -3,5 +3,10 @@
 from harmonize import *
 
 def error(sample):
-    '''Calculate the error rate of the algorithm over the given sample.'''
-    pass
+    'Calculate the error rate of the algorithm over the given sample.'
+    p = Freq() # frequency table for whether a classification is correct or incorrect
+    for v in voices(sample): # TODO
+        cs = harmonize(list(sample.notes(v)), sample.key, v)[1]
+        for c, c1 in zip(sample.chords(), cs):
+            p[c == c1] += 1
+    return p[True, False] # probability of failure
