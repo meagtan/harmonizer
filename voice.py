ec15d8f675759a347a6381fb8ff03dad644db81b
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2017-03-10 14:29:54 +0300

    Added voice leading definition

---
+++ voice.py
@@ -0,0 +1,10 @@
+### Generate an optimal voice leading sequence for a sequence of chords
+
+import freqs
+from music21 import *
+
+def voiceleading(chords, melody = None):
+	'''
+	Generate voice leading for a sequence of chords, optionally accompanying a melody.
+	'''
+	pass
