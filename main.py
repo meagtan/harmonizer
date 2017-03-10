a9fee0cab6899effa865c73c38baf73e5272d87e
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2017-03-10 14:33:41 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+### Command line interface for functions
+
+import freqs
+import harmonize
+import error
+import enharmonic
+import voice
+
+# 1. Take input from file or user
+
+# 2. Call desired function on input, specifying type of output (show, save to file, etc.)
+
+# 3. Display result
