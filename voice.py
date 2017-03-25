b14c77e59f59f6133def92627db978c81a195d79
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2017-03-25 08:20:43 +0300

    Implemented voiceleading

--- voice.py
+++ voice.py
@@ -28,26 +28,26 @@ class VoiceEnv(Env):
 		self.mfreq = Freq()	# frequency table for types of motion, taking Motion, current Tones, current Func, Key
 	
     def process(self, sample):
-        'Update probabilities based on a sample sequence of chords.'
-        if sample in self.samples:
-            return
-        
-        vel = sample.vel
-        key = sample.key
-        self.kfreq[key] += 1
-        
-        # add sample to samples
-        self.samples.add(sample)
-        
-        prev = None
-        for curr in sample.chords():
-            f, f1 = Func(curr, key), Func(prev, key)
-            self.cfreq[f, sample] += 1     
-            self.tfreq[f, f1, sample] += 1 
-            for voice, n in enumerate(curr.pitches): # TODO sort pitches first
+		'Update probabilities based on a sample sequence of chords.'
+		if sample in self.samples:
+		    return
+		
+		vel = sample.vel
+		key = sample.key
+		self.kfreq[key] += 1
+		
+		# add sample to samples
+		self.samples.add(sample)
+		
+		prev = None
+		for curr in sample.chords():
+			f, f1 = Func(curr, key), Func(prev, key)
+			self.cfreq[f, sample] += 1     
+			self.tfreq[f, f1, sample] += 1 
+			for voice, n in enumerate(curr.pitches): # TODO sort pitches first
 				t = Tone(n, curr)
 				self.nfreq[t, f, voice, sample] += 1
-                try:
+				try:
 					n1 = prev.pitches[voice] # what if multiple notes are played on the same chord? consider ties
 					t1 = Tone(n1, prev)
 					self.vfreq[t, t1, f, f1, voice, sample] += 1
@@ -57,20 +57,57 @@ class VoiceEnv(Env):
 						m1 = prev.pitches[v1]
 						tm, tm1 = Tone(m, curr), Tone(m1, prev)
 						self.mfreq[Motion(tn, tm, tn1, tm1), tm, tn, f, sample] += 1	# should there be more conditionals?
-                except:
-                    pass
-            prev = curr
+				except:
+				    pass
+			prev = curr
 	
-	def mprob(n1, m1, n, m, c1, c, k):
+	def mprob(self, n1, m1, n, m, c1, c, k):
 		'mprob(n1, m1, n, m, c1, c, k) -> The probability of notes (n, m) and chord c moving to (n1, m1) and c1 in key k.'
 		f, f1 = Func(c, k), Func(c1, k)
 		tn, tn1, tm, tm1 = Tone(n, c), Tone(n1, c1), Tone(m, c), Tone(m1, c1)
 		return self.mfreq[True, Motion(tn1, tm1, tn, tm), tm1, tn1, f1]
 
-def voiceleading(chords, melody = None):
-	'Generate voice leading for a sequence of chords, optionally accompanying a melody.'
-	pass
+    
+    
+    def voiceleading(self, chords, key, melody = None):
+    	'Generate voice leading for a sequence of chords, optionally accompanying a melody.'
+        # Viterbi algorithm
+        probs  = defaultdict(Prob)
+        preds  = {}
+        length = len(chords)
+        
+        for ns, p in self.voicings(chord):
+            probs[ns, 0] = p
+        
+        for i in xrange(1, length):
+            bestprob = Prob()
+            best = None
+            for ns, _ in self.voicings(chords[i], k):
+                # find max and arg max of transition
+                for ns1, _ in self.voicings(chords[i-1], k):
+                    newprob = probs[ns1, i - 1] * self.voicingprob(ns, chords[i], k, ns1, chords[i-1])
+                    if probs[ns, i] < newprob:
+                        probs[ns, i] = newprob
+                        preds[ns, i] = ns1
+                # update bestprob and bestchor
+                if bestprob < probs[ns, i]:
+                    bestprob = probs[ns, i]
+                    best = ns
+        
+        # construct voice leading
+        ns = best
+        voices = [ns]
+        for i in xrange(length - 1, 0, -1):
+            ns = preds[ns, i]
+            voices.append(ns)
+        voices.reverse()
+        
+        return voices, bestprob
 
-def voicings(chord, prev = None):
-	'Generate voicings of a chord, optionally coming after another chord.'
-	pass
+    def voicings(self, chord, key, prev = None):
+    	'Generate voicings of a chord, optionally coming after another chord, along with their probabilities.'
+    	pass
+    
+    def voicingprob(self, ns, c, k, ns1 = None, c1 = None):
+        'Return the probability of a voicing of a chord in a given key, optionally after another chord.'
+        pass
