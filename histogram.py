19122a1106262e5134fa54a531b4f965963850f1
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2017-04-02 05:19:42 +0300

    Create histogram.py

---
+++ histogram.py
@@ -0,0 +1,11 @@
+### Histogram analysis for easier recognition of keys, chords, themes, etc.
+
+import freqs
+
+def findkey(s):
+    'Return the most likely key of a score.'
+    pass
+
+def histogram(s):
+    'Construct a pitch histogram for a score.'
+    pass
