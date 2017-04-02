e9bd0ec9172d8e6c5f481dbdebeff77bdbef6a53
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2017-04-02 05:58:04 +0300

    Implemented findkey and histogram

--- histogram.py
+++ histogram.py
@@ -1,11 +1,18 @@
 ### Histogram analysis for easier recognition of keys, chords, themes, etc.
 
 import freqs
+from music21 import pitch
+from math import log
 
-def findkey(s):
-    'Return the most likely key of a score.'
-    pass
+def findkey(table, s):
+    'Return the most likely key of a sample, given probability table.'
+    h = histogram(s)
+    return max(freqs.keys, key = lambda k: sum(log(table.nfreq[True, freqs.Tone(n, k)]) * h[n] for n in h))
 
 def histogram(s):
-    'Construct a pitch histogram for a score.'
-    pass
+    'Construct a pitch histogram for a sample.'
+    h = freqs.Freq()
+    for c in s.chords():
+        for n in c.pitches:
+            h[pitch.Pitch(n.name), n.octave] += 1 # separate octave from pitch
+    return h
