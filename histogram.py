fbc357f567e29ae74250ab2b6ae1b399e0431c32
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2017-04-02 17:26:46 +0300

    Added Krumhansl-Schmuckler and SVM classifiers for keys

--- histogram.py
+++ histogram.py
@@ -3,6 +3,7 @@
 import freqs
 from music21 import pitch
 from math import log
+from svmutil import * # for keysvm, requires libsvm
 
 def findkey(table, s):
     'Return the most likely key of a sample, given probability table.'
@@ -16,3 +17,29 @@ def histogram(s):
         for n in c.pitches:
             h[pitch.Pitch(n.name), n.octave] += 1 # separate octave from pitch
     return h
+
+# Other histogram-based classifiers against which to measure the accuracy of findkey
+
+def findkeyks(s):
+    'Return the most likely key of a sample, using the Krumhansl-Schmuckler algorithm.'
+    return s.cs.analyze('krumhansl')
+
+class keysvm():
+    'SVM classifier for finding the most likely key of a sample from its histogram.'
+    
+    def __init__(self, samples):
+        'Train SVM from given set of samples.'
+        x, y = [], []
+        for s in samples:
+            h = histogram(s)
+            y.append(freqs.keys.index(s.key)) # labels of multi-class classifier correspond to indices of keys
+            x.append(dict((freqs.tones.index(str(n)), h[n]) for n in h)) # each dimension corresponds to the index of a note
+        prob = svm_problem(y, x)
+        param = svm_parameter('-t 0 -v 5 -q')
+        self.m = svm_train(prob, param)
+    
+    def findkey(self, s):
+        'Predict the key of a sample based on the trained SVM classifier.'
+        h = histogram(s)
+        x0, _ = gen_svm_nodearray(dict((freqs.tones.index(str(n)), h[n]) for n in h)) # TODO make this into a function
+        return freqs.keys[int(libsvm.svm_predict(self.m, x0))]
