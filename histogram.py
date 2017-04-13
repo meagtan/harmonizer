0be650541109a58f5af8983649ae7173eccc38d3
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2017-04-13 05:23:35 +0300

    Implemented boundaries

--- histogram.py
+++ histogram.py
@@ -93,26 +93,24 @@ def modulation(table, s, n):
     Identify modulation in a sample s by dividing it into at most n regions.
     '''
     w = {} # w[t, r] corresponds to the weight of time t for region r
-    c = [] # c[r] corresponds to the class weight of region r
-    m = [] # m[r] corresponds to the class mean of region r
-    k = [] # k[r] corresponds to the most likely key for region r
-    b = [] # b[r] corresponds to the boundaries of region r
+    c = [1] * n # c[r] corresponds to the class weight of region r
+    m = [1] * n # m[r] corresponds to the class mean of region r
+    k = [1] * n # k[r] corresponds to the most likely key for region r
     s = 1  # s is the common class variance, initially set to 1
     l = len(s.cs) # total duration of sample
     km = findkey(table, s) # key of whole piece
     
     # initially, divide sample evenly
     for r in xrange(n):
-        c[r] = 1
         m[r] = (r + 0.5) * l / n
     
     # loop until convergence
-    for _ in xrange(10): # TODO better condition
+    for _ in xrange(10): # TODO better condition, such as keys not changing
         # find b[r] from c[r] and m[r]
-        b = boundaries(c, m)
-        for r in xrange(n):
+        b = boundaries(c, m, s, l)
+        for r in b:
             # calculate k[r] from b[r]
-            k[r] = findkey(table, s[b[r][0]:b[r][1]]) # TODO implement slicing for Sample
+            k[r] = findkey(table, s[b[r][0]:b[r][1]]) # or perhaps compute k[r] from matrix product of s and w
             # calculate weights
             for t in xrange(l):
                 w[t, r] = c[r] * exp(-((t-m[r])/s) ** 2 + loglikelihood(table, s[t:t+1], k[r]) - loglikelihood(table, s[t:t+1], km))
@@ -121,8 +119,28 @@ def modulation(table, s, n):
             m[r] = sum(t * w[t,r] for t, r1 in w if r == r1) / sum(w[t,r] for t, r1 in w if r == r1)
             m1 = sum(t * w[t,r1] for t, r1 in w) / sum(w[t,r1] for t, r1 in w) # sample mean
             s = sum((t - m1) ** 2 * w[t,r1] for t, r1 in w) / sum(w[t,r] for t, r1 in w)
-    return boundaries(c, m)
+    return boundaries(c, m, s, l)
 
-def boundaries(c, m):
+def boundaries(c, m, s, l):
     'Calculate region boundaries from region means and weights.'
-    pass
+    def discr(t, r):
+        'Linear discriminant for classifying a time instant to a region.'
+        return 2 * m[r] * t - m[r] ** 2 + s ** 2 * log(c[r])
+    def intersect(r, r1):
+        'The time the discriminants of two regions intersect.'
+        # 2(m[r]-m[r1])t - m[r]^2 + m[r1]^2 + s^2 log(c[r]/c[r1]) = 0
+        return (m[r] + m[r1]) / 2 - s ** 2 * log(c[r]/c[r1]) / (2 * (m[r] - m[r1]))
+    b = {}
+    t = 0
+    n = len(c)
+    
+    # find most likely region in the beginning, hopefully region 0
+    r = max(xrange(n), key = lambda r: discr(t, r))
+    while t < l:
+        # find the region first to overcome the discriminant of the current region and set it to the next region
+        # perhaps optimize this through dynamic programming
+        r1, t1 = min((r1, intersect(r, r1)) for r1 in xrange(n) if intersect(r, r1) > t, key = lambda a: a[1])
+        b[r] = t, t1
+        r, t = r1, t1
+    b[r] = t, l - 1
+    return b
